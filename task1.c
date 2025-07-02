043b69aef10a0c50e0a16ce44be1884ad1d4f531
Daniil Breslavskyi <danosnyt2115@gmail.com>
2025-07-02 15:55:20 +0200

    add a main function

--- task1.c
+++ task1.c
@@ -3,32 +3,29 @@
 #include <time.h>
 
 void pozmen_hodnoty_zachovajuc_priemer(float *p, int l){
+	
+    float sum = 0;
+	float rand_num[l];
+    float rand_sum = 0;
+    float rand_avg = 0;
     
     if(l==0){
         return;
     }
-    
-    float sum = 0;
-    
+        
     for(int i = 0; i < l; i++){
         sum += p[i];
     }
     
-    float rand_num[l];
-    float rand_sum = 0;
-    float rand_avg = 0;
-    
-    srand(time(NULL);
-
     for(int i = 0; i < l; i++){
-        rand_num[i] = (float(rand() % 200 ) -100 );    
+        rand_num[i] = ((float(rand() % 200.0 )) -100.0 );    
     }
     
     for(int i = 0; i < l; i++){
         rand_sum += rand_num[i];
     }
-     
-	rand_avg = rand_sum / l;
+
+	rand_avg = rand_sum/l;
 
     for(int i = 0; i < l; i++){
         rand_num[i] - rand_avg;
@@ -40,3 +37,24 @@ void pozmen_hodnoty_zachovajuc_priemer(float *p, int l){
 
 }
 
+int main(){
+	
+	float pole[]={3.7 5.3, 7.3, 180.9, 55.2};
+	int l = sizeof(pole);
+	int i = 0;
+	
+    srand(time(NULL);
+    
+	for(i = 0; i < l; i++){
+		printf("%.2f\n", pole[i]);
+	}
+	
+	pozmen_hodnoty_zachovajuc_priemer(pole, l);
+	
+	printf("new one:\n")
+	for(i = 0; i < l; i++){
+		printf("%.2f\n", pole[i]);
+	}
+	
+	return 0;
+}
